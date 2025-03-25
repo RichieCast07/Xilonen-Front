b2744daa7967e002b069d796ac38e8e29c300d78
RichieCast07 <richardaton234@gmail.com>
2025-03-24 22:34:47 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { GraphicsComponent } from './dashboard/graphics/graphics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: GraphicsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraphicsComponent } from './graphics/graphics.component';
+
+
+
+@NgModule({
+  declarations: [
+    GraphicsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
